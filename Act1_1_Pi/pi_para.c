d9c6c426d99a3fdbd9cbf43a8bf5b2cc765c4bf9
OshkarVTec <a01275287@tec.mx>
2022-11-18 19:53:42 -0600

    edited number of threads

--- Act1_1_Pi/pi_para.c
+++ Act1_1_Pi/pi_para.c
@@ -2,7 +2,7 @@
 #include <omp.h>
 static long num_pasos=10000000;
 double paso;
-#define NUM_THREADS 50
+#define NUM_THREADS 100
 void main(){
  int i, nthreads;
  double pi, sum[NUM_THREADS], t1, t2, tiempo;
